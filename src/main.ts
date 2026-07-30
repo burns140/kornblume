dcd2592ad67739e8bce40475f098366f9f89fef9
Stephen Burns <stephen.burns230@gmail.com>
2026-07-30 15:52:09 -0400

    revert formatting changes in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import "./assets/main.css";
-import "flag-icons/css/flag-icons.min.css";
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./stores";
-import i18n from "./utils/i18n";
-import GoogleSignInPlugin from "vue3-google-signin";
+import './assets/main.css';
+import 'flag-icons/css/flag-icons.min.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './stores';
+import i18n from './utils/i18n';
+import GoogleSignInPlugin from 'vue3-google-signin';
 
 const app = createApp(App);
 app.use(store);
@@ -14,7 +14,7 @@ app.use(i18n);
 
 // please create a .env file in the root of the project
 app.use(GoogleSignInPlugin, {
-  clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID || "mock-client-id",
+    clientId: import.meta.env.VITE_GOOGLE_CLIENT_ID || 'mock-client-id'
 });
 
-app.mount("#app");
+app.mount('#app');
